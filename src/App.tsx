5303cdeae124796447542d7c86575a13ab536f39
Caio Rodrigues <54294014+caiorodr@users.noreply.github.com>
2023-02-04 15:40:37 -0300

    create pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 import { ThemeProvider } from "styled-components"
-import { Button } from "./components/Button"
 import { GlobalStyle } from "./styles/global"
 import { defaultTheme } from "./styles/themes/default"
 
 export function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary"/>
-      <Button variant="secondary"/>
-      <Button variant="success"/>
-      <Button variant="danger"/>
-      
+    <ThemeProvider theme={defaultTheme}>      
       <GlobalStyle/>
     </ThemeProvider>
     

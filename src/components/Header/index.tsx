c55d9dec2677213130c972eaca83a2cd3742cec3
Caio Rodrigues <54294014+caiorodr@users.noreply.github.com>
2023-02-27 19:10:08 -0300

    criação do timer

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,20 @@
 import { HeaderContainer } from "./styled";
 
+import logo from '../../assets/Logo.svg';
+import { Scroll, Timer } from "phosphor-react";
+import { NavLink } from "react-router-dom";
+
 export function Header() {
     return (
         <HeaderContainer>
-            <span>Logo</span>
+            <img src={logo} alt="" />
             <nav>
-                <a href="">timer</a>
-                <a href="">history</a>
+                <NavLink to="/" title="Timer">
+                    <Timer size={24}/>
+                </NavLink>
+                <NavLink to="/history" title="Histórico">
+                    <Scroll size={24}/>
+                </NavLink>
             </nav>
         </HeaderContainer>
     )

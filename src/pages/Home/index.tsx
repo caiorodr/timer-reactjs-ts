4d00673b0c6606afea04085b244f3b8fef5819f9
Caio Rodrigues <54294014+caiorodr@users.noreply.github.com>
2023-03-10 14:16:47 -0300

    criação do historico

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import { Play } from "phosphor-react";
-import { CountdownContainer, FormContainer, HomeContainer, Separator } from "./styles";
+import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from "./styles";
 
 export function Home () {
     return (
@@ -7,10 +7,24 @@ export function Home () {
             <form>
                 <FormContainer>
                     <label htmlFor="task">Vou trabalhar em</label>
-                    <input id="task" />
-
+                    <TaskInput id="task" list="task-suggestions" placeholder="Dê um nome para o seu projeto" />
+                    
+                    <datalist id="task-suggestions">
+                        <option value="Projeto1"/>
+                        <option value="Projeto2"/>
+                        <option value="Projeto3"/>
+                        <option value="Banana"/>
+                    </datalist>
+                   
                     <label htmlFor="">durante</label>
-                    <input type="number" id="minutesAmaunt" />
+                    <MinutesAmountInput 
+                        type="number" 
+                        id="minutesAmaunt" 
+                        placeholder="00" 
+                        step={5}
+                        min={5}
+                        max={60}
+                    />
 
                     <span>minutos.</span>
                 </FormContainer>
@@ -23,10 +37,10 @@ export function Home () {
                     <span>0</span>
                 </CountdownContainer>
 
-                <button type="submit">
+                <StartCountdownButton disabled type="submit">
                     <Play size={24}/>
                     Começar
-                </button>
+                </StartCountdownButton>
             </form>
         </HomeContainer>
     )
